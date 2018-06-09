e316a47d8b749bcf9e51f449a23a3ca95995d3be
RarCeth <rarceth@gmail.com>
2018-06-09 11:53:38 +0800

    fh

--- GAM404_Assignment1/Assets/CreationController.cs
+++ GAM404_Assignment1/Assets/CreationController.cs
@@ -2,101 +2,107 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-public class CreationController : MonoBehaviour {
-
-    public Button createButton;
-
-    public InputField usernameInput, levelInput;
-    public Dropdown classInput;
-
-    public Text finalUsername, finalLevel, finalExperience, finalClass;
-
-    public List<Character> createdCharacters = new List<Character>();
-	// Use this for initialization
-	void Start () {
-        createButton.onClick.AddListener(() => CreateCharacter());
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-
-    public void CreateCharacter()
+namespace Week1
+{
+    public class CreationController : MonoBehaviour
     {
-        string u = usernameInput.text;
-        int l = 0;
-        bool tryLevel = int.TryParse(levelInput.text, out l);
-        Character.Type c;
-        int tryClass = classInput.value;
-        switch (tryClass)
+
+        public Button createButton;
+
+        public InputField usernameInput, levelInput;
+        public Dropdown classInput;
+
+        public Text finalUsername, finalLevel, finalExperience, finalClass;
+
+        public List<Character> createdCharacters = new List<Character>();
+        // Use this for initialization
+        void Start()
         {
-            case 0:
-                c = Character.Type.Knight;
-                break;
-            case 1:
-                c = Character.Type.Mage;
-                break;
-            case 2:
-                c = Character.Type.Rogue;
-                break;
-            default:
-                c = Character.Type.Knight;
-                break;
+            createButton.onClick.AddListener(() => CreateCharacter());
         }
 
-        Character newCharacter = new Character()
+        // Update is called once per frame
+        void Update()
         {
-            Username = u,
-            Level = l,
-            Class = c,
-            currentXP = 0,
-            targetXP = l * 123
+
+        }
+
+        public void CreateCharacter()
+        {
+            string u = usernameInput.text;
+            int l = 0;
+            bool tryLevel = int.TryParse(levelInput.text, out l);
+            Character.Type c;
+            int tryClass = classInput.value;
+            switch (tryClass)
+            {
+                case 0:
+                    c = Character.Type.Knight;
+                    break;
+                case 1:
+                    c = Character.Type.Mage;
+                    break;
+                case 2:
+                    c = Character.Type.Rogue;
+                    break;
+                default:
+                    c = Character.Type.Knight;
+                    break;
+            }
+
+            Character newCharacter = new Character()
+            {
+                Username = u,
+                Level = l,
+                Class = c,
+                currentXP = 0,
+                targetXP = l * 123
+            };
+
+            createdCharacters.Add(newCharacter);
+
+            finalUsername.text = u;
+            finalLevel.text = l.ToString();
+            finalClass.text = c.ToString();
+            finalExperience.text = newCharacter.currentXP + "/" + newCharacter.targetXP;
+        }
+    }
+
+    [System.Serializable]
+    public class Character
+    {
+        public string Username;
+        public int Level;
+        public int currentXP, targetXP;
+        public enum Type
+        {
+            Knight,
+            Rogue,
+            Mage
         };
+        public Type Class;
 
-        createdCharacters.Add(newCharacter);
-
-        finalUsername.text = u;
-        finalLevel.text = l.ToString();
-        finalClass.text = c.ToString();
-        finalExperience.text = newCharacter.currentXP + "/" + newCharacter.targetXP;
-    }
-}
-
-[System.Serializable]
-public class Character
-{
-    public string Username;
-    public int Level;
-    public int currentXP, targetXP;
-    public enum Type
-    {
-        Knight, 
-        Rogue,
-        Mage
-    };
-    public Type Class;
-
-    public Character ()
-    {
-
-    }
-
-    public Character (int random)
-    {
-        string[] usernames = new string[]
+        public Character()
         {
+
+        }
+
+        public Character(int random)
+        {
+            string[] usernames = new string[]
+            {
             "Gorok",
             "Ushnark",
             "Argie",
             "Gordon"
-        };
+            };
 
-        Username = usernames[Random.Range(0, usernames.Length)];
-        Level = Random.Range(0, 10);
-        currentXP = Random.Range(0, 1000);
-        targetXP = Random.Range(currentXP, 10000);
-        Class = Type.Knight;
+            Username = usernames[Random.Range(0, usernames.Length)];
+            Level = Random.Range(0, 10);
+            currentXP = Random.Range(0, 1000);
+            targetXP = Random.Range(currentXP, 10000);
+            Class = Type.Knight;
+        }
     }
 }
+
---
+++ GAM404_Assignment1/Assets/StatisticsController.cs
@@ -0,0 +1,395 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Week3
+{
+    public class StatisticsController : MonoBehaviour
+    {
+        public int levelConstant, balanceModifier;
+        public List<Character> allCharacters = new List<Character>();
+        public List<Character> teamOne = new List<Character>();
+        public List<Character> teamTwo = new List<Character>();
+
+        public bool fightOnStart;
+        // Use this for initialization
+        void Start()
+        {
+            PopulateRoster();
+            AssignTeams();
+            BalanceTeams();
+            if (fightOnStart)
+            {
+                DoBattle();
+            }
+            
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void PopulateRoster()
+        {
+            allCharacters.Clear();
+            for (int i = 0; i < 15; i++)
+            {
+                allCharacters.Add(new Character(levelConstant)
+                {
+                    ID = i
+                });
+            }
+        }
+
+        public void AssignTeams()
+        {
+            List<Character> aCharacters = new List<Character>(allCharacters);
+
+            teamOne.Clear();
+            teamTwo.Clear();
+
+            for (int i = 0; i < 5; i++)
+            {
+                Character toAdd = aCharacters[Random.Range(0, aCharacters.Count - 1)];
+                toAdd.team = Character.Team.One;
+                teamOne.Add(toAdd);
+                aCharacters.Remove(toAdd);
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Character toAdd = aCharacters[Random.Range(0, aCharacters.Count - 1)];
+                toAdd.team = Character.Team.Two;
+                teamTwo.Add(toAdd);
+                aCharacters.Remove(toAdd);
+            }
+        }
+
+        public void BalanceTeams ()
+        {
+            foreach (Character c in teamTwo)
+            {
+                c.Recalculate(balanceModifier);
+            }
+        }
+
+        public void CompleteBattleRound()
+        {
+            string win = "";
+            List<DamageResult> roundResults = new List<DamageResult>();
+            foreach (Character c in teamOne)
+            {
+                if (teamTwo.Count > 0)
+                {
+                    Character target = GetTarget(teamTwo, c);
+                    roundResults.Add(DamageTarget(target, c, teamTwo));
+                }
+                else
+                {
+                    win = "TEAM ONE WINNER";
+                    break;
+                }
+                
+            }
+            if (win == "")
+            {
+                foreach (Character c in teamTwo)
+                {
+                    if (teamOne.Count > 0)
+                    {
+                        Character target = GetTarget(teamOne, c);
+                        roundResults.Add(DamageTarget(target, c, teamOne));
+                    }
+                    else
+                    {
+                        win = "TEAM TWO WINNER";
+                        break;
+                    }
+
+                }
+            }
+            
+
+            string result = "";
+            result += "TEAM ONE: " + teamOne.Count + " | TEAM TWO: " + teamTwo.Count + "\n";
+            foreach (DamageResult r in roundResults)
+            {
+                result += r.aggressor.team + " vs " + r.defender.team + ". DMG: " + r.damage + ".";
+                if (r.defenderDead)
+                {
+                    result += " Defender killed";
+                }
+                result += "\n";
+            }
+            result += "\n " + win;
+            print(result);
+        }
+
+        [ContextMenu("Do Battle")]
+        public void DoBattle ()
+        {
+            int roundCounter = 0;
+            while (teamOne.Count > 0 && teamTwo.Count > 0)
+            {
+                CompleteBattleRound();
+                roundCounter++;
+            }
+            print("Rounds to win: " + roundCounter);
+        }
+        //Modify targets not teamTwo... duh XD
+        public Character GetTarget(List<Character> targets, Character c)
+        {
+            Character target = null;
+            switch (c.attackType)
+            {
+                case Character.AttackType.Random:
+                    target = teamTwo[Random.Range(0, teamTwo.Count - 1)];
+                    break;
+                case Character.AttackType.LowestHealth:
+                    float bigHealth = Mathf.Infinity;
+                    foreach (Character ch in teamTwo)
+                    {
+                        if (ch.statistics.health < bigHealth)
+                        {
+                            bigHealth = ch.statistics.health;
+                            target = ch;
+                        }
+                    }
+                    break;
+                case Character.AttackType.HighestHealth:
+                    float lowHealth = -Mathf.Infinity;
+                    foreach (Character ch in teamTwo)
+                    {
+                        if (ch.statistics.health > lowHealth)
+                        {
+                            lowHealth = ch.statistics.health;
+                            target = ch;
+                        }
+                    }
+                    break;
+                case Character.AttackType.LowestDP:
+                    float bigDP = Mathf.Infinity;
+                    foreach (Character ch in teamTwo)
+                    {
+                        if (ch.statistics.damage < bigDP)
+                        {
+                            bigDP = ch.statistics.damage;
+                            target = ch;
+                        }
+                    }
+                    break;
+                case Character.AttackType.HighestDP:
+                    float lowDP = -Mathf.Infinity;
+                    foreach (Character ch in teamTwo)
+                    {
+                        if (ch.statistics.damage > lowDP)
+                        {
+                            lowDP = ch.statistics.damage;
+                            target = ch;
+                        }
+                    }
+                    break;
+            }
+            return target;
+        }
+
+        public DamageResult DamageTarget(Character target, Character origin, List<Character> targetTeam)
+        {
+            bool killingBlow = false;
+            int dmg = origin.statistics.damage;
+
+            float rnd = Random.Range(0, 100);
+            if (rnd < (origin.statistics.hitChance - target.statistics.dodgeChance))
+            {
+                target.statistics.health -= origin.statistics.damage;
+                if (target.statistics.health <= 0)
+                {
+                    targetTeam.Remove(target);
+                    killingBlow = true;
+                }
+            } 
+            else
+            {
+                dmg = 0;
+            }
+
+
+
+            DamageResult result = new DamageResult()
+            {
+                aggressor = origin,
+                defender = target,
+                defenderDead = killingBlow,
+                damage = dmg
+            };
+
+            return result;
+        }
+    }
+
+    [System.Serializable]
+    public class Character
+    {
+        //Base Attributes
+        public AttributeSet attributes;
+        //Resultant Statistics
+        public StatisticsSet statistics;
+
+        public enum Team
+        {
+            One,
+            Two
+        };
+
+        public Team team;
+
+        public enum AttackType
+        {
+            Random,
+            LowestHealth,
+            LowestDP,
+            HighestHealth,
+            HighestDP
+        }
+
+        public AttackType attackType;
+
+        public int ID;
+
+        public Character(int lvl)
+        {
+            attributes = new AttributeSet(lvl);
+            statistics = new StatisticsSet(attributes, lvl);
+        }
+
+        public void Recalculate (int changeInLevel)
+        {
+            attributes.AddToAttributes(changeInLevel);
+            statistics.GenerateStatistics(attributes, statistics.level + changeInLevel);
+        }
+
+    }
+
+    [System.Serializable]
+    public class AttributeSet
+    {
+        public int dex, sta, str, acu;
+        //public delegate void OnGenerateAttributes();
+        //public OnGenerateAttributes onGenerateAttributes;
+
+        public AttributeSet(int lvl)
+        {
+            GenerateAttributes(lvl);
+        }
+
+        public void GenerateAttributes(int lvl)
+        {
+            int counter = 0;
+
+            dex = 0;
+            sta = 0;
+            str = 0;
+            acu = 0;
+
+            while (counter < lvl)
+            {
+                float f = Random.Range(0, 100);
+                if (f > 75)
+                {
+                    dex++;
+                }
+                else if (f > 50)
+                {
+                    sta++;
+                }
+                else if (f > 25)
+                {
+                    str++;
+                }
+                else if (f > 0)
+                {
+                    acu++;
+                }
+                counter++;
+            }
+            
+
+            //onGenerateAttributes();
+        }
+
+        public void AddToAttributes (int levelsToAdd)
+        {
+            int counter = 0;
+
+            while (counter < levelsToAdd)
+            {
+                float f = Random.Range(0, 100);
+                if (f > 75)
+                {
+                    dex++;
+                }
+                else if (f > 50)
+                {
+                    sta++;
+                }
+                else if (f > 25)
+                {
+                    str++;
+                }
+                else if (f > 0)
+                {
+                    acu++;
+                }
+                counter++;
+            }
+        }
+    }
+
+    [System.Serializable]
+    public class StatisticsSet
+    {
+        public int level;
+        public float xpCurrent, xpTarget;
+        public int dodgeChance, health, damage, hitChance;
+
+        public StatisticsSet()
+        {
+
+        }
+
+        public StatisticsSet(AttributeSet att, int lvl)
+        {
+            GenerateStatistics(att, lvl);
+        }
+
+        public void GenerateStatistics(AttributeSet att, int lvl)
+        {
+            level = lvl;
+            //Notes on algorithm.
+            //Set up a base level. The numbers are hardcoded, i know, but ive been tinkering to get nice values out of them. 
+            //The base values let every character have at least a certain level of a statistic. 
+            //From there, add advantages from point assignation. 
+            float dg = (level * 2) + (att.dex * 3.5f);
+            float hp = (level * 10f) + (att.sta * 7.5f);
+            float dmg = (level * 5) + (att.str * 3.5f);
+            float hit = (level * 4.5f) + (att.acu * 8);
+
+            dodgeChance = (int)dg;
+            health = (int)hp;
+            damage = (int)dmg;
+            hitChance = (int)hit;
+        }
+    }
+
+    public class DamageResult
+    {
+        public Character aggressor, defender;
+        public bool defenderDead;
+        public int damage;
+
+    }
+
+}
+
